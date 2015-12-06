42a9324697cd43802a53a9eeffcd8b428b11ad99
Stanley Stuart <stanley@stan.li>
2015-12-06 14:02:53 -0600

    implement class extracting

---
+++ test/converter_test.js
@@ -0,0 +1,69 @@
+'use strict';
+
+var convert = require('../lib/converter');
+var document = require('./fixtures/ember-data-docs');
+var assert = require('chai').assert;
+var _ = require('lodash');
+
+let findClasses = (document) => document.data.filter(doc => doc.type === 'class');
+
+describe('converting to a jsonapi document', function(){
+
+  beforeEach(function(){
+    this.model = convert(document);
+  });
+
+  afterEach(function() {
+    this.model = null;
+  });
+
+  describe('classes', function() {
+
+    it('extracts all the classes', function() {
+      let classCount = Object.keys(document.classes).length;
+      let classes = findClasses(this.model);
+
+      assert.equal(classCount, classes.length);
+    });
+
+    it('adds the attributes to the `attributes` field', function() {
+      let CLASS_ATTRIBUTES = ['name', 'shortname', 'file', 'line', 'description'];
+      let classes = findClasses(this.model);
+
+      classes.forEach(klass => {
+        CLASS_ATTRIBUTES.forEach(attr => assert.property(klass.attributes, attr));
+      });
+    });
+
+    it('adds descendants to the relationships objects', function() {
+      let classes = findClasses(this.model);
+
+      let parentClasses = _.uniq(_.values(document.classes)
+                            .filter(klass => klass.extends && !/^Ember/.exec(klass.extends))
+                            .map(klass => klass.extends))
+                            .sort();
+
+      let classesWithDescendants = _.uniq(classes
+          .filter(klass => klass.relationships.descendants.data.length)
+          .map(klass => klass.attributes.name))
+          .sort();
+
+      assert.deepEqual(parentClasses, classesWithDescendants);
+    });
+
+    it('adds parentClass to the relationships object', function() {
+      let classes = findClasses(this.model);
+
+      let childrenFromFixtures = _.values(document.classes)
+                                 .filter(klass => klass.extends);
+
+      childrenFromFixtures.forEach(klass => {
+        let model = _.find(classes, (c) => c.id === klass.name);
+        assert.ok(model);
+        assert.equal(model.relationships.parentClass.data.id, klass.extends);
+      });
+    });
+  });
+
+});
+
